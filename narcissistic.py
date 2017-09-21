ce09a2c7ee37383e78c436a4f5a12f6e162bc5d7
lovefov <654747873@163.com>
2017-09-21 11:27:07 +0800

    python practise 水仙花数

---
+++ narcissistic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author:贾江超
+def narcissistic(value):
+    strNum = str(value)
+    l = len(strNum)
+    num = int(strNum)
+    sum = 0
+    for i in range(0, l):
+        a = int(strNum[i]) ** l
+        sum = a + sum
+    print(sum == num)
