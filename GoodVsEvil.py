32359cd0a28d9f2f799c15e87546a7f7a90c2c81
lovefov <654747873@163.com>
2017-09-26 02:41:39 +0800

    python demo

--- GoodVsEvil.py
+++ GoodVsEvil.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-#-*- coding:utf-8 -*-
-#Author:贾江超
+# -*- coding:utf-8 -*-
+# Author:贾江超
+
 
 def goodVsEvil(good, evil):
-    arrgood=[i for i in list(good) if i !=' ']
-    arrbad=[i for i in list(evil) if i !=' ']
-    goodvalue=[1,2,3,3,4,10]
-    badvalue=[1,2,2,2,3,5,10]
-    sumgood=0
-    sumbad=0
-    for i in range(6):
-        sumgood+=(int(arrgood[i]))*goodvalue[i]
-    for j in range(7):
-        sumbad+=(int(arrbad[j]))*badvalue[j]
+    arrgood = [i for i in list(good) if i != ' ']
+    arrbad = [i for i in list(evil) if i != ' ']
+    goodvalue = [1, 2, 3, 3, 4, 10]
+    badvalue = [1, 2, 2, 2, 3, 5, 10]
+    sumgood = sum([int(x) * y for x, y in zip(good.split(), goodvalue)])
+    sumbad= sum([int(x) * y for x, y in zip(evil.split(), badvalue)])
     if sumgood > sumbad:
         return 'Battle Result: Good triumphs over Evil'
     if sumgood < sumbad:
@@ -20,3 +17,19 @@ def goodVsEvil(good, evil):
     if sumgood == sumbad:
         return "Battle Result: No victor on this battle field"
 
+
+def goodVsEvil(good, evil):
+    points_good = [1, 2, 3, 3, 4, 10]
+    points_evil = [1, 2, 2, 2, 3, 5, 10]
+
+    good = sum([int(x) * y for x, y in zip(good.split(), points_good)])
+    evil = sum([int(x) * y for x, y in zip(evil.split(), points_evil)])
+
+    result = 'Battle Result: '
+
+    if good < evil:
+        return result + 'Evil eradicates all trace of Good'
+    elif good > evil:
+        return result + 'Good triumphs over Evil'
+    else:
+        return result + 'No victor on this battle field'
