a9f2d2fce5ff1ccc86ffc7f772de8ecf1ca2bbb7
lovefov <654747873@163.com>
2017-09-26 05:23:27 +0800

    python demo

--- HumanReadableDurationFormat.py
+++ HumanReadableDurationFormat.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# Author:贾江超
-
 def format_duration(seconds):
     YEARS = 31536000
     DAYS = 86400
@@ -9,7 +5,7 @@ def format_duration(seconds):
     MINS = 60
     if seconds % YEARS == 0:
         if int(seconds / YEARS) == 1:
-            return str(int(seconds / YEARS)) + ' year'
+            return '1 year'
         elif int(seconds / YEARS) == 0:
             return 'now'
         else:
@@ -18,30 +14,56 @@ def format_duration(seconds):
         year = int(seconds / YEARS // 1)
     else:
         year = 0
+    print(year)
     if (seconds - YEARS * year) % DAYS == 0:
-
-        if int((seconds - YEARS * year) / DAYS // 1) == 1:
-            return str(int((seconds - YEARS * year) / DAYS // 1)) + ' day'
+        if year==0:
+            if int((seconds - YEARS * year) / DAYS // 1) == 1:
+                return '1 day'
+            else:
+                return str(int((seconds - YEARS * year) / DAYS // 1)) + ' days'
         else:
-            return str(int((seconds - YEARS * year) / DAYS // 1)) + ' days'
+            if int((seconds - YEARS * year) / DAYS // 1) == 1:
+                return str(year)+'1 day'
+            else:
+                return str(int((seconds - YEARS * year) / DAYS // 1)) + ' days'
     elif (seconds - YEARS * year) > DAYS:
         day = int((seconds - YEARS * year) / DAYS // 1)
     else:
         day = 0
+    print(day)
     if (seconds - YEARS * year - DAYS * day) % HOURS == 0:
-        if int((seconds - YEARS * year - DAYS * day) / HOURS // 1) == 1:
-            return str(int((seconds - YEARS * year - DAYS * day) / HOURS // 1)) + ' hour'
-        else:
-            return str(int((seconds - YEARS * year - DAYS * day) / HOURS // 1)) + ' hours'
+        if year==0 and day==0:
+            if int((seconds - YEARS * year - DAYS * day) / HOURS // 1) == 1:
+                return '1 hour'
+            else:
+                return str(int((seconds - YEARS * year - DAYS * day) / HOURS // 1)) + ' hours'
+        elif year==0 and day!=0:
+            if int((seconds - YEARS * year - DAYS * day) / HOURS // 1) == 1:
+                return str(day)+'1 hour'
+            else:
+                return str(day)+str(int((seconds - YEARS * year - DAYS * day) / HOURS // 1)) + ' hours'
+        elif year!=0 and day==0:
+            if int((seconds - YEARS * year - DAYS * day) / HOURS // 1) == 1:
+                return str(year)+str(day) + '1 hour'
+            else:
+                return str(year)+str(day) + str(int((seconds - YEARS * year - DAYS * day) / HOURS // 1)) + ' hours'
+
     elif seconds - YEARS * year - DAYS * day > HOURS:
         hour = int((seconds - YEARS * year - DAYS * day) / HOURS // 1)
     else:
         hour = 0
     if (seconds - YEARS * year - DAYS * day - HOURS * hour) % MINS == 0:
-        if int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1) == 1:
-            return str(int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1)) + ' minute'
+        if year==0 and day==0 and hour==0:
+            if int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1) == 1:
+                return '1 minute'
+            else:
+                return str(int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1)) + ' minutes'
         else:
-            return str(int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1)) + ' minutes'
+            if int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1) == 1:
+                return str(year)+str(day)+str(hour)+'1 minute'
+            else:
+                return str(year)+' years, '+str(day)+' days, '+str(hour)+' hours and '+str(int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1)) + ' minutes'
+
     elif seconds - YEARS * year - DAYS * day - HOURS * hour > MINS:
         mins = int((seconds - YEARS * year - DAYS * day - HOURS * hour) / MINS // 1)
     else:
@@ -53,39 +75,19 @@ def format_duration(seconds):
         else:
             return str(sec) + ' seconds'
     arr = [year, day, hour, mins, sec]
-    sample = ['year', 'day', 'hour', 'minute', 'second']
-    samples = ['years', 'days', 'hours', 'minutes', 'seconds']
+    sample = ['year, ', 'day, ', 'hour, ', 'minute and ', 'second']
+    samples = ['years, ', 'days, ', 'hours, ', 'minutes and ', 'seconds']
     count = 0
     rearr = []
     for i in arr:
         if i == 1:
             string = str(i) + ' ' + sample[count]
-            if sample[count] == 'day':
-                string = str(i) + ' ' + sample[count] + ', '
-            if sample[count] == 'hour':
-                string = str(i) + ' ' + sample[count] + ', '
-            if sample[count] == 'minute':
-                string = str(i) + ' ' + sample[count] + ' ' + 'and '
-
             rearr.append(string)
             count += 1
         elif i > 1:
             string = str(i) + ' ' + samples[count]
-            if sample[count] == 'days':
-                string = str(i) + ' ' + samples[count] + ', '
-            if sample[count] == 'hour':
-                string = str(i) + ' ' + samples[count] + ', '
-            if sample[count] == 'minutes':
-                string = str(i) + ' ' + samples[count] + ' ' + 'and '
-
             rearr.append(string)
             count += 1
         else:
             count += 1
     return ''.join(rearr)
-
-
-
-format_duration(3601)
-
-
